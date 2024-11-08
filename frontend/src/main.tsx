30d42c41137618dbe5aa446a1bf0e703d938485c
Nicolas Gutierrez <nicogusuarez@gmail.com>
2024-11-08 14:28:15 +0100

    Fixed CI/CD

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -65,7 +65,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(  
   <StrictMode>
     <FirebaseAppProvider firebaseConfig={config} suspense>
       <Flowbite theme={{ theme: flowbiteTheme }}>
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,9 +1,10 @@
-import { getAuthInstance } from "@/utils/firebase/firebaseConfig";
+import { config, getAuthInstance } from "@/utils/firebase/firebaseConfig";
 import { useFirebaseApp, AuthProvider } from "@/utils/reactfire";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
   const app = useFirebaseApp();
+  console.log(config);
 
   return (
     <AuthProvider sdk={getAuthInstance(app)}>
