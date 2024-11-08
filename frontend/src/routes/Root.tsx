8a76dcb4b5be51cca0e98f1a838454426c1f70d2
Nicolas Gutierrez <nicogusuarez@gmail.com>
2024-11-08 16:59:13 +0100

    Analytics

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,13 +1,24 @@
 import { getAuthInstance } from "@/utils/firebase/firebaseConfig";
-import { useFirebaseApp, AuthProvider } from "@/utils/reactfire";
+import {
+  useFirebaseApp,
+  AuthProvider,
+  AnalyticsProvider,
+} from "@/utils/reactfire";
+import { getAnalytics } from "firebase/analytics";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
   const app = useFirebaseApp();
 
-  return (
+  const content = (
     <AuthProvider sdk={getAuthInstance(app)}>
       <Outlet />
     </AuthProvider>
+  );
+
+  return import.meta.env.DEV ? (
+    content
+  ) : (
+    <AnalyticsProvider sdk={getAnalytics(app)}>{content}</AnalyticsProvider>
   );
 }
