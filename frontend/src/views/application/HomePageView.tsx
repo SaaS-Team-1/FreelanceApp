15ce406e98406644be43ea803c873d4430a3bfbe
AyaCh21 <aya.chaounibenabdallah@student.kuleuven.be>
2024-11-19 12:59:38 +0100

    commented out gig list

--- frontend/src/views/application/HomePageView.tsx
+++ frontend/src/views/application/HomePageView.tsx
@@ -5,7 +5,6 @@ import PostedGigListHome from "@/components/Gigs/PostedGigListHome";
 import { Timestamp } from "firebase/firestore";
 import { Gig, User } from "@/utils/database/schema";
 import { NotificationItemProps } from "@/components/Notifications/NotificationItem";
-import { FaEuroSign } from "react-icons/fa"; // Import the icon for the price badge if needed
 
 export default function OverviewView() {
   const notifications: NotificationItemProps[] = [
@@ -28,8 +27,84 @@ export default function OverviewView() {
       time: "1m ago",
       count: 2,
     },
-    // Additional notifications...
+    {
+      user: {
+        email: "amina.agile@example.com",
+        displayName: "Amina Agile",
+        profile: {
+          bio: "Computer Science Student in Leuven City.",
+          credits: 100,
+          picture: "",
+          location: "Leuven",
+        },
+        completedGigs: ["gig1", "gig2"],
+        activeGigs: ["gig3"],
+        listedGigs: ["gig4", "gig5"],
+        averageRating: 4.5,
+      },
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      time: "1m ago",
+      count: 2,
+    },
+    {
+      user: {
+        email: "amina.agile@example.com",
+        displayName: "Amina Agile",
+        profile: {
+          bio: "Computer Science Student in Leuven City.",
+          credits: 100,
+          picture: "",
+          location: "Leuven",
+        },
+        completedGigs: ["gig1", "gig2"],
+        activeGigs: ["gig3"],
+        listedGigs: ["gig4", "gig5"],
+        averageRating: 4.5,
+      },
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      time: "1m ago",
+      count: 2,
+    },
+    {
+      user: {
+        email: "amina.agile@example.com",
+        displayName: "Amina Agile",
+        profile: {
+          bio: "Computer Science Student in Leuven City.",
+          credits: 100,
+          picture: "",
+          location: "Leuven",
+        },
+        completedGigs: ["gig1", "gig2"],
+        activeGigs: ["gig3"],
+        listedGigs: ["gig4", "gig5"],
+        averageRating: 4.5,
+      },
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      time: "1m ago",
+      count: 2,
+    },
+    {
+      user: {
+        email: "amina.agile@example.com",
+        displayName: "Amina Agile",
+        profile: {
+          bio: "Computer Science Student in Leuven City.",
+          credits: 100,
+          picture: "",
+          location: "Leuven",
+        },
+        completedGigs: ["gig1", "gig2"],
+        activeGigs: ["gig3"],
+        listedGigs: ["gig4", "gig5"],
+        averageRating: 4.5,
+      },
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      time: "1m ago",
+      count: 2,
+    },
   ];
+  
 
   const user: User = {
     email: "amina.agile@example.com",
@@ -85,8 +160,8 @@ export default function OverviewView() {
     },
   ];
 
-  // Sample list of gigs by other users for display
-  const postedGigs = gigs.map(gig => ({ gig, lister: user }));
+  /* // Sample list of gigs by other users for display
+  const postedGigs = gigs.map(gig => ({ gig, lister: user })); */
 
   return (
     <div className="flex flex-1 flex-col space-y-6 p-6">
@@ -109,14 +184,15 @@ export default function OverviewView() {
             </button>
           </div>
         </div>
-
+       /*
         {/* Main content section containing scrollable gigs */}
-        <div className="flex flex-col justify-center space-y-8">
+       {/*  <div className="flex flex-col space-y-6">
           {/* Scrollable Posted Gigs List */}
-          <PostedGigListHome gigs={postedGigs} showSeeMoreButton={true} />
-        </div>
+         {/* <PostedGigListHome gigs={postedGigs} showSeeMoreButton={true} /></div> 
+          */}
       </div>
-    </div>
+    </div> 
+
 
     {/* Fixed Right Column for Notifications and Posted Gigs */}
     <div className="fixed  right-0 flex h-screen w-1/4 flex-col space-y-6 p-6" style={{ bottom: "2px" }}>
