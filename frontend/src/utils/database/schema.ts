fa467af981a571122257959825e3544e0411fff8
salmaloukili <salma.loukili@student.kuleuven.be>
2024-11-22 14:19:03 +0100

    new schema + fixed sidebar username

--- frontend/src/utils/database/schema.ts
+++ frontend/src/utils/database/schema.ts
@@ -1,70 +1,3 @@
-// import { Timestamp } from "firebase/firestore";
-
-// export interface User {
-//   email: string;
-//   displayName: string;
-//   profile: {
-//     bio: string;
-//     credits: number;
-//     picture?: string;
-//     location: string;
-//   };
-//   completedGigs: string[];
-//   activeGigs: string[];
-//   listedGigs: string[];
-//   averageRating: number;
-// }
-
-// export interface Gig {
-//   title: string;
-//   description: string;
-//   category: string;
-//   price: number;
-//   dueDate: Timestamp;
-//   status: "open" | "in-progress" | "completed";
-//   listerId: string;
-//   selectedApplicantId?: string;
-//   createdAt: Timestamp;
-//   applicantIds: string[];
-// }
-
-// export interface Application {
-//   gigId: string;
-//   applicantId: string;
-//   listerId: string;
-//   status: "pending" | "accepted" | "rejected";
-//   appliedAt: Timestamp;
-//   coverLetter: string;
-// }
-
-// export interface Message {
-//   applicationId: string;
-//   senderId: string;
-//   receiverId: string;
-//   lastSent: Timestamp;
-// }
-
-// export interface Rating {
-//   toUserId: string;
-//   byUserId: string;
-//   byUserDisplayName: string;
-//   rating: number;
-//   review: string;
-//   createdAt: Timestamp;
-// }
-
-// export interface Transaction {
-//   senderId: string;
-//   receiverId: string;
-//   gigId: string;
-//   amount: number;
-//   createdAt: Timestamp;
-// }
-
-
-
-
-/////////////////////////////////////////  OPTIMIZED SCHEMA (NEEDS REVIEWING) //////////////////////////////
 
 
 // make sure lister cant delete gig until they cancel gig first, which requires applicant's confirmation
@@ -88,9 +21,6 @@ export interface User {
     completedGigs: number; 
     averageRating: number; 
   };
-  listedGigs: string[]; // gig IDs
-  activeApplications: string[]; // application IDs (for gigs they applied to that are still open)
-  chatIds: string[]; // chat IDs where the user is a participant
 }
 
 
@@ -98,25 +28,24 @@ export interface Gig {
   gigId: string;
   title: string;
   description: string;
+  location: string;
   category: string;
   price: number;
   dueDate: Timestamp;
-  status: "open" | "in-progress" | "awaiting-confirmation" | "completed" | "rated" | "canceled" | "deleted" | "problem-reported"; 
+  status: "open" | "in-progress" | "awaiting-confirmation" | "completed" | "deleted" | "problem-reported"; 
   listerId: string;
   selectedApplicantId?: string;
-  applications: string[]; // list of application IDs
   createdAt: Timestamp;
-  ratingId?: string[]; // rating ID if the gig is completed and rated
+  ratingId?: string; // rating ID if the gig is completed and rated
 }
 // gig status is what the lister sees in the chat
 // open = gig is posted and you haven't assigned it to anyone yet
 // deleted = you deleted gig 
 // in-progress = gig has been assigned to [selectedApplicant]
-// canceled = you previously assigned this gig to this user but you mutually canceled it, wait for applicant to confirm
 // awaiting-confirmation = applicant has reported to have completed the gig, now you need to confirm to release the payement 
 // completed = gig is completed and confirmed but you haven't rated applicant
 // problem-reported = you have reported a problem with the applicant's work
-// rated = gig completed and rated --> see rating / list new gig
+
 
 
 export interface Application {
@@ -125,7 +54,7 @@ export interface Application {
   applicantId: string;
   listerId: string;
   coverLetter: string;
-  status: "pending" | "discarded" | "canceled" | "assigned" | "awaiting-lister-completion" | "completed" | "rated" ;
+  status: "pending" | "discarded" |  "assigned" | "awaiting-lister-completion" | "completed" ;
   appliedAt: Timestamp;
   chatId: string;
 }
@@ -133,29 +62,26 @@ export interface Application {
 // no application = apply to gig
 // pending = you have applied to this gig but lister hasnt assigned it to you or anyone else
 // discarded = lister has either deleted gig or assigned it to someone else
-// canceled = you or lister have canceled your application
 // assigned = lister assigned gig to you, added to your schedule and you are working on it / will show up on the scheduled day
 // awaiting-lister-completion = you have finished the gig work and have reported the gig to be completed, now wait for lister's confirmation
 // completed = gig marked completed from both sides and your money had been transferred to your wallet, but you haven't rated the lister yet
-// rated = you have completed gig and rated the lister
+
 
 
 
 export interface Chat {
   chatId: string;
   gigId: string;
-  participants: string[]; // (applicant and lister)
-  applicationID: string;
-  messages: ChatMessage[];
+  userId: string; // (applicant and lister)
+  applicationId?: string;
 }
 
 
 export interface ChatMessage {
-  messageId: string; 
   senderId: string; 
   content: string; 
   timestamp: Timestamp; 
-  isRead: boolean;
+  chatId: string;
 }
 
 
