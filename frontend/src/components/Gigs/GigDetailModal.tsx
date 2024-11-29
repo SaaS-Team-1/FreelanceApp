c9209f8a0f06cb8d302536618bbb868ffee8abf0
Aymeric Maixent Phonsavanh Baume <aymericmaixentphonsavanh.baume@student.kuleuven.be>
2024-11-29 14:56:06 +0100

    application bg click closes it

--- frontend/src/components/Gigs/GigDetailModal.tsx
+++ frontend/src/components/Gigs/GigDetailModal.tsx
@@ -103,122 +103,127 @@ const GigDetailModal: React.FC<GigDetailModalProps> = ({
 
   return ReactDOM.createPortal(
     <>
-      <div className="fixed inset-0 bg-black/50 backdrop-blur-sm"></div>
-      <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="w-3/4 max-w-4xl overflow-hidden rounded-lg bg-gray-800 p-8">
-          <div className="pr-4">
-            {/* Lister Profile Picture and Name */}
-            <div className="mb-4 flex items-center gap-4">
-              <UserProfilePicture
-                user={lister}
-                size="large" // Display larger profile picture
-                hoverDetails={true} // Show hover details
-              />
+      <div
+        className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50"
+        onClick={onClose}
+      >
+        <div className="fixed inset-0 bg-black/50 backdrop-blur-sm"></div>
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="w-3/4 max-w-4xl overflow-hidden rounded-lg bg-gray-800 p-8">
+            <div className="pr-4">
+              {/* Lister Profile Picture and Name */}
+              <div className="mb-4 flex items-center gap-4">
+                <UserProfilePicture
+                  user={lister}
+                  size="large" // Display larger profile picture
+                  hoverDetails={true} // Show hover details
+                />
+                <div>
+                  <h3
+                    className="w-fit text-nowrap text-lg font-bold text-white"
+                    style={{
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      maxWidth: "200px", // Adjust this to your desired max width
+                    }}
+                  >
+                    {lister.displayName}
+                  </h3>
+                  {lister.profile.location && (
+                    <p className="text-sm text-gray-400">
+                      {lister.profile.location}
+                    </p>
+                  )}
+                </div>
+                <h2 className="text-2xl font-bold text-white">{gig.title}</h2>
+              </div>
+
+              <p className="mb-4 text-gray-300">{gig.description}</p>
+
+              <div className="justify-left mb-6 flex flex-col gap-6 text-sm text-white sm:flex-row">
+                <div className="flex flex-col items-center">
+                  <div className="flex items-center">
+                    <FaDollarSign className="mr-2" />
+                    <span>{gig.price ? `${gig.price} Tokens` : "TBD"}</span>
+                  </div>
+                  <span className="ml-4 text-xs text-gray-400">Price</span>
+                </div>
+                <div className="flex flex-col items-center">
+                  <div className="flex items-center">
+                    <FaCalendarAlt className="mr-2" />
+                    <span>
+                      {gig.dueDate
+                        ? `${new Date(
+                            gig.dueDate.seconds * 1000,
+                          ).toLocaleDateString("en-GB")}`
+                        : "N/A"}
+                    </span>
+                  </div>
+                  <span className="ml-2 text-xs text-gray-400">Due Date</span>
+                </div>
+                <div className="flex flex-col items-center">
+                  <div className="flex items-center">
+                    <FaMapMarkerAlt className="mr-2" />
+                    <span>{location}</span>
+                  </div>
+                  <span className="ml-6 text-xs text-gray-400">Location</span>
+                </div>
+              </div>
+
               <div>
-                <h3
-                  className="w-fit text-nowrap text-lg font-bold text-white"
-                  style={{
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    maxWidth: "200px", // Adjust this to your desired max width
-                  }}
-                >
-                  {lister.displayName}
-                </h3>
-                {lister.profile.location && (
-                  <p className="text-sm text-gray-400">
-                    {lister.profile.location}
-                  </p>
-                )}
-              </div>
-              <h2 className="text-2xl font-bold text-white">{gig.title}</h2>
-            </div>
-
-            <p className="mb-4 text-gray-300">{gig.description}</p>
-
-            <div className="justify-left mb-6 flex flex-col gap-6 text-sm text-white sm:flex-row">
-              <div className="flex flex-col items-center">
-                <div className="flex items-center">
-                  <FaDollarSign className="mr-2" />
-                  <span>{gig.price ? `${gig.price} Tokens` : "TBD"}</span>
+                <h4 className="text-sm font-semibold text-gray-400">Tags:</h4>
+                <div className="mt-2 flex gap-2">
+                  <Badge
+                    label={gig.category}
+                    color="beige"
+                    textColor="black"
+                    outline={true}
+                    outlineColor="beige"
+                    rounded={true}
+                    size="small"
+                  />
+                  <Badge
+                    label={location}
+                    color="beige"
+                    textColor="black"
+                    outline={true}
+                    outlineColor="beige"
+                    rounded={true}
+                    size="small"
+                  />
+                  <Badge
+                    label={`${gig.price ? `${gig.price} Tokens` : "TBD"}`}
+                    color="beige"
+                    textColor="black"
+                    outline={true}
+                    outlineColor="beige"
+                    rounded={true}
+                    size="small"
+                  />
                 </div>
-                <span className="ml-4 text-xs text-gray-400">Price</span>
               </div>
-              <div className="flex flex-col items-center">
-                <div className="flex items-center">
-                  <FaCalendarAlt className="mr-2" />
-                  <span>
-                    {gig.dueDate
-                      ? `${new Date(
-                          gig.dueDate.seconds * 1000,
-                        ).toLocaleDateString("en-GB")}`
-                      : "N/A"}
-                  </span>
-                </div>
-                <span className="ml-2 text-xs text-gray-400">Due Date</span>
-              </div>
-              <div className="flex flex-col items-center">
-                <div className="flex items-center">
-                  <FaMapMarkerAlt className="mr-2" />
-                  <span>{location}</span>
-                </div>
-                <span className="ml-6 text-xs text-gray-400">Location</span>
-              </div>
-            </div>
 
-            <div>
-              <h4 className="text-sm font-semibold text-gray-400">Tags:</h4>
-              <div className="mt-2 flex gap-2">
-                <Badge
-                  label={gig.category}
-                  color="beige"
-                  textColor="black"
-                  outline={true}
-                  outlineColor="beige"
-                  rounded={true}
-                  size="small"
-                />
-                <Badge
-                  label={location}
-                  color="beige"
-                  textColor="black"
-                  outline={true}
-                  outlineColor="beige"
-                  rounded={true}
-                  size="small"
-                />
-                <Badge
-                  label={`${gig.price ? `${gig.price} Tokens` : "TBD"}`}
-                  color="beige"
-                  textColor="black"
-                  outline={true}
-                  outlineColor="beige"
-                  rounded={true}
-                  size="small"
-                />
-              </div>
-            </div>
-
-            <div className="mt-4 flex justify-between">
-              <CustomButton
-                label="Close"
-                onClick={onClose}
-                color="red"
-                textColor="black"
-                size="medium"
-                rounded={true}
-              />
-              {userId && db && (
+              <div className="mt-4 flex justify-between">
                 <CustomButton
-                  label="Apply"
-                  onClick={handleApply} // Trigger the apply function
-                  color="primary"
-                  textColor="white"
+                  label="Close"
+                  onClick={onClose}
+                  color="red"
+                  textColor="black"
                   size="medium"
                   rounded={true}
                 />
-              )}
+                {userId && db && (
+                  <CustomButton
+                    label="Apply"
+                    onClick={handleApply} // Trigger the apply function
+                    color="primary"
+                    textColor="white"
+                    size="medium"
+                    rounded={true}
+                  />
+                )}
+              </div>
             </div>
           </div>
         </div>
