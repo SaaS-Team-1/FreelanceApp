e93ac30a04b76791fffaaed75eb8f1aeeddd69a2
Shusaku1 <segashu0214@gmail.com>
2024-12-03 16:58:34 +0100

    added gig fixture based on schema, added a check of firestore after creating a gig

--- frontend/cypress/e2e/gig.cy.ts
+++ frontend/cypress/e2e/gig.cy.ts
@@ -20,6 +20,7 @@ describe("Flow of Posting a Gig", () => {
         cy.loginUser1(); 
         cy.log("User 1 logged in");
     });
+
     // User 1 post a gig
     it("User 1 posts a gig", function () {
         cy.get('.h-screen > :nth-child(2) > .flex').click();
@@ -28,22 +29,54 @@ describe("Flow of Posting a Gig", () => {
         // type in gig details
         cy.get('.space-y-4 > :nth-child(1) > .w-full').type(this.gigs.gig1.title);
         cy.get('.h-40').type(this.gigs.gig1.description);
-        cy.get(':nth-child(3) > :nth-child(1) > .w-full').type('{backspace}').type(this.gigs.gig1.price);
+        cy.get(':nth-child(3) > :nth-child(1) > .w-full')
+            .clear() 
+            .type('150')
+            .type('{del}');
         cy.get(':nth-child(3) > :nth-child(2) > .w-full').type(this.gigs.gig1.location);
         cy.get(':nth-child(4) > .w-full').type(this.gigs.gig1.category);
         cy.get(':nth-child(5) > :nth-child(1) > .w-full').type(this.gigs.gig1.dueDate.split("T")[0]);
         cy.get(':nth-child(5) > :nth-child(2) > .w-full').type(this.gigs.gig1.dueDate.split("T")[1].substring(0, 5));
+        cy.get('button.bg-blue-500').contains('Create Gig').click();
+        cy.on('window:alert', (text) => {
+            expect(text).to.equal('Gig successfully created.');
+        });
+        cy.callFirestore('add', 'test_hello_world', { some: 'value' });
+        cy.wait(2000);
     });
+
+    it("Confirm if posted gig is in firestore", function () {
+        // Check Firestore to verify the gig is added
+        cy.callFirestore("get", "gigs").then((gigs) => {
+            const createdGig = gigs.find(gig => gig.title === this.gigs.gig1.title);
+            expect(createdGig).to.exist;
+            expect(createdGig.description).to.equal(this.gigs.gig1.description);
+            expect(createdGig.price).to.equal(this.gigs.gig1.price);
+            cy.log("Gig verified in Firestore.");
+
+            cy.log("User 1 created a gig completely");
+        });
+    });
+    
+        
+
 
     it("User 1 logs out", () => {
         cy.logout();
     });
     
-    
-    
+    it("Create and login User2", function () {
+        cy.createUser2();
+        cy.loginUser2(); 
+        cy.log("User 2 logged in");
+    });
+
+    it("User 2 logs out", () => {
+        cy.logout();
+    });
     // User 2 login and apply for the gig
     // User 1 checks the gig and assign User 2 to the gig
     // User 1 checks if the gig is applied by User 2
     // User 1 assigns User 2 to the gig
 
-})
+});
