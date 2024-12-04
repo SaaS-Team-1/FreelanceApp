9a7bdf5fd28264014f01f9cac7001a3663138fb8
Aymeric Maixent Phonsavanh Baume <aymericmaixentphonsavanh.baume@student.kuleuven.be>
2024-12-04 15:27:49 +0100

    refactored ProfileView

--- frontend/src/views/application/ProfileView.tsx
+++ frontend/src/views/application/ProfileView.tsx
@@ -1,23 +1,14 @@
-import { useLoaderData } from "react-router-dom";
 import { useUser, useFirestore } from "@/utils/reactfire";
 import { User } from "@/utils/database/schema";
 import { useEffect, useState } from "react";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import UserProfilePicture from "@/components/Avatar/UserProfilePicture";
 
-export interface UserParams {
-  UID?: string;
-}
 
-export async function userLoader({ params }: { params: UserParams }) {
-  return params;
-}
-
-export default function UserView() {
+export default function ProfileView() {
   const { data: userU } = useUser();
   const db = useFirestore();
   const [user, setUser] = useState<User | null>(null);
-  const { UID } = useLoaderData() as UserParams;
 
   const fetchUser = async () => {
     try {
@@ -50,34 +41,35 @@ export default function UserView() {
   }
 
   return (
-    <div className="bg-gray-900 p-6 text-gray-200">
+    <div className="scrollbar mx-auto flex h-screen w-full flex-col items-center space-y-10 overflow-y-scroll py-10 lg:overflow-y-hidden">
+      <div className="flex items-center justify-center space-x-6 lg:w-full">
+        <div className="justify-self-end rounded-lg bg-gray-800 p-4 shadow-md"></div>
+      </div>
       <div className="mx-auto max-w-4xl rounded-lg bg-gray-800 p-6 shadow-md">
         <div className="mb-6 flex items-center justify-between">
-        <div className="mb-4 flex items-center gap-4">
-                <UserProfilePicture
-                  user={user}
-                  size="large" // Display larger profile picture
-                  hoverDetails={true} // Show hover details
-                />
-                <div>
-                  <h3
-                    className="w-fit text-nowrap text-lg font-bold text-white"
-                    style={{
-                      whiteSpace: "nowrap",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      maxWidth: "200px", // Adjust this to your desired max width
-                    }}
-                  >
-                    {user.displayName}
-                  </h3>
-                  {user.profile.location && (
-                    <p className="text-sm text-gray-400">
-                      {user.profile.location}
-                    </p>
-                  )}
-                </div>
-              </div>
+          <div className="mb-4 flex items-center gap-4">
+            <UserProfilePicture
+              user={user}
+              size="large" // Display larger profile picture
+              hoverDetails={true} // Show hover details
+            />
+            <div>
+              <h3
+                className="w-fit text-nowrap text-lg font-bold text-white"
+                style={{
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  maxWidth: "200px", // Adjust this to your desired max width
+                }}
+              >
+                {user.displayName}
+              </h3>
+              {user.profile.location && (
+                <p className="text-sm text-gray-400">{user.profile.location}</p>
+              )}
+            </div>
+          </div>
           <div className="flex flex-col items-end">
             <button className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700">
               Edit Profile
