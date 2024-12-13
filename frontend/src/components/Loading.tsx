ec05382946ff17d9725a6d47586f7e9ebe25fcef
Nicolas Gutierrez <nicogusuarez@gmail.com>
2024-12-13 11:01:54 +0100

    Fixed loading

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -3,17 +3,19 @@ import ReactLoading from "react-loading";
 export default function Loading() {
   return (
     <div className="flex size-full flex-col items-center justify-center">
-      <ReactLoading
-        className="mb-10"
-        type={"spinningBubbles"}
-        delay={1}
-        height={"20%"}
-        width={"20%"}
-        color="black"
-      />
-      <h2 className="mb-6 self-center text-2xl font-extrabold leading-none tracking-tight text-primary sm:text-5xl">
-        Loading
-      </h2>
+      <div className="relative flex items-center justify-center">
+        <div className="absolute">
+          <h2 className="text-2xl mt-56 font-extrabold leading-none tracking-tight text-primary sm:text-5xl">
+            Loading
+          </h2>
+        </div>
+        <ReactLoading
+          type="spinningBubbles"
+          delay={1}
+          width="300px"
+          color="#415f91"
+        />
+      </div>
     </div>
   );
 }
