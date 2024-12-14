2966304c6ec835defd7b97924fc0a28bcbf70d8b
Nicolas Gutierrez <nicogusuarez@gmail.com>
2024-12-14 16:13:00 +0100

    Last changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import { setGlobalOptions } from "firebase-functions/options";
-setGlobalOptions({ region: "europe-west1" });
+setGlobalOptions({ region: "europe-west1", enforceAppCheck: true });
 
 import * as stripe from "./stripe";
 import * as common from "./common";
